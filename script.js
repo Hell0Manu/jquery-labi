a850a63f2f56ed177701c28d15d6abdbc6156d46
Emanuelle <102062744+Hell0Manu@users.noreply.github.com>
2024-09-01 17:32:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $('a').click(function () {
+        $('section').hide();
+
+        var sectionToShow = $(this).attr('href');
+        $(sectionToShow).show();
+
+        if (sectionToShow === "#sobre") {
+            $('#sobre h2').addClass('green-theme');
+        }
+
+        if (sectionToShow === "#equipe") {
+            $('#equipe h2').addClass('blue-theme');
+            $('#equipe h3').addClass('blue-theme');
+        }
+
+        if (sectionToShow === "#agenda") {
+            $('#agenda h2').addClass('red-theme');
+        }
+
+        if (sectionToShow === "#projeto") {
+            $('#projeto h2').addClass('brown-theme');
+            $('#projeto h3').addClass('brown-theme');
+        }
+
+        if (sectionToShow === "#contato") {
+            $('#contato h2').addClass('yellow-theme');
+            $('#contato span').addClass('yellow-theme');
+        }
+    });
+
+    $('#active').click(function () {
+        $('section').show();
+    })
+
+    $('.topnav').click(function() {
+        $('.content-menu').toggleClass('active');
+    });
+    
+
+});
+
+
+
